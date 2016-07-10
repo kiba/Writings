d0bbf301a45c99b6a6aa1d50d6843445218fe35e
Kiba <hackerkiba@gmail.com>
2016-07-10 08:28:45 -0400

    generate directory

--- bin/report.rb
+++ bin/report.rb
@@ -2,6 +2,7 @@ directories = ["for_science","atg"]
 
 def generateDir dir
   if Dir.exist?("reports/" + dir) == false
-    puts "directory does not exists"
+    puts "directory does not exists. Generating directory: " + dir
+    Dir.mkdir("reports/" + dir)
   end
 end
