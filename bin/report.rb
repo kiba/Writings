d72cd6b4fd1b79579442bbe4ccb112f894e418d4
Kiba <hackerkiba@gmail.com>
2016-07-10 08:31:58 -0400

    conform to ruby style guide

--- bin/report.rb
+++ bin/report.rb
@@ -1,6 +1,6 @@
 directories = ["for_science","atg"]
 
-def generateDir dir
+def generate_dir dir
   if Dir.exist?("reports/" + dir) == false
     puts "directory does not exists. Generating directory: " + dir
     Dir.mkdir("reports/" + dir)
