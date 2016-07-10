9f9bfda6fe2e3a5786f0ad04f164bf9f07936e6e
Kiba <hackerkiba@gmail.com>
2016-07-10 08:25:40 -0400

    forgot to use a method

--- bin/report.rb
+++ bin/report.rb
@@ -1,7 +1,7 @@
 directories = ["for_science","atg"]
 
 def generateDir dir
-  if Dir("reports/" + dir) == false
+  if Dir.exist?("reports/" + dir) == false
     puts "directory does not exists"
   end
 end
