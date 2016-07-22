d3406c1494587b606a2ba567fc23df9dab37ef17
Kiba <hackerkiba@gmail.com>
2016-07-22 15:32:06 -0400

    figure out how to work with options

--- bin/changes.rb
+++ bin/changes.rb
@@ -6,8 +6,8 @@ directories = ["for_science","atg"]
 options = {}
 OptionParser.new do |opts|
   opts.banner = "Usage: changes.rb [options]"
-  opts.on("-t", "--today", "Sum up today's changes compared to last available report.") do |a|
-    options[:all] = a
+  opts.on("-t", "--today", "Sum up today's changes compared to last available report.") do |t|
+    options[:all] = t
   end
 
   opts.on("-h", "--help", "Print all commands.") do
@@ -15,5 +15,6 @@ OptionParser.new do |opts|
   end
 end.parse!
 
-p options
-p ARGV
+if options[:all] == true
+  puts "beep"
+end
