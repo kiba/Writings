4fb858f50925ae56a81cdb051cfa433517953719
Kiba <hackerkiba@gmail.com>
2016-07-22 15:24:12 -0400

    add an option to work with

--- bin/changes.rb
+++ bin/changes.rb
@@ -3,4 +3,10 @@ require "optparse"
 options = {}
 OptionParser.new do |opts|
   opts.banner = "Usage: changes.rb [options]"
+  opts.on("-a", "--all", "Sum up all changes") do |a|
+    options[:all] = a
+  end
 end.parse!
+
+p options
+p ARGV
