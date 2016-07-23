97b3d22a224281d73c6ee89b5f480ca54402ac65
Kiba <hackerkiba@gmail.com>
2016-07-23 15:44:24 -0400

    rename an option

--- bin/changes.rb
+++ bin/changes.rb
@@ -7,7 +7,7 @@ options = {}
 OptionParser.new do |opts|
   opts.banner = "Usage: changes.rb [options]"
   opts.on("-t", "--today", "Sum up today's changes compared to last available report.") do |t|
-    options[:all] = t
+    options[:today] = t
   end
 
   opts.on("-h", "--help", "Print all commands.") do
@@ -15,7 +15,7 @@ OptionParser.new do |opts|
   end
 end.parse!
 
-if options[:all] == true
+if options[:today] == true
   today = Date.today
   filename = "reports/for_science/" + today.to_s + ".md"
   if File.exist?(filename)
