b2f0aac6584347f8ca75f09a40d71d7b37d520c0
Kiba <hackerkiba@gmail.com>
2016-08-21 11:36:20 -0400

    use module rather than class

--- bin/report.rb
+++ bin/report.rb
@@ -1,11 +1,12 @@
 #!/usr/bin/env ruby
 require "date"
 
-
-def generate_dir dir
-  if Dir.exist?("reports/" + dir) == false
-    puts "directory does not exists. Generating directory: " + dir
-    Dir.mkdir("reports/" + dir)
+module Report
+  def generate_dir dir
+    if Dir.exist?("reports/" + dir) == false
+      puts "directory does not exists. Generating directory: " + dir
+      Dir.mkdir("reports/" + dir)
+    end
   end
 end
 
